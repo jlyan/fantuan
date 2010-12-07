7c367e78c756a9e2dc8172db6fb000cd66dc8a05
yan <jinlai.yan@gmail.com>
2010-12-07 22:52:20 +0800

    Initial commit

---
+++ fantuan/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fantuan::Application.config.secret_token = '1cd585c094bffddc32785542c60ffc3e932e426d81720d90b898b752547b33ff1939acced87484007bfc34c941b9b72a32460d340d7cd0a0da20eaf52cf02e20'
